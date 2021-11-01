309c54dff4e1a92a054c1dd791c0db997016550b
GabrielHGobi <67982371+GabrielHGobi@users.noreply.github.com>
2021-11-01 18:58:36 -0300

    Cleaned NoticesScreen code // added Header

--- src/screens/NoticesScreen.js
+++ src/screens/NoticesScreen.js
@@ -3,20 +3,17 @@ import {
   View,
   StyleSheet,
   Text,
-  ImageBackground,
-  Image,
   ScrollView,
   TouchableOpacity,
 } from "react-native";
 
+import Header from "../components/Header";
 import NoticeCard from "../components/NoticeCard";
 import TypeButtonOverlay from "../components/TypeButtonOverlay";
 import DateButtonOverlay from "../components/DateButtonOverlay";
 
 import { Context as NoticesContext } from "../context/NoticesContext";
 
-const logo_casdvest = require("../../assets/logo_casdvest.png");
-const MessageScreen = "Message";
 const NoticesScreen = ({ navigation }) => {
   const { state, getMessages } = useContext(NoticesContext);
 
@@ -30,18 +27,14 @@ const NoticesScreen = ({ navigation }) => {
       listener.remove();
     };
   }, []);
+
   const [pressedType, setPressedType] = useState(false);
   const [pressedDate, setPressedDate] = useState(false);
+
   return (
     <>
-      <ImageBackground //TODO: fix the background
-        style={styles.containerBackground}
-        imageStyle={styles.imageBackground}
-        resizeMode='cover'>
-        <View style={styles.logoContainer}>
-          <Image source={logo_casdvest} style={styles.logo} />
-          <Text style={styles.logoText}>Mural de Avisos</Text>
-        </View>
+      <View style={styles.containerBackground}>
+        <Header title='Mural de avisos' />
         <View style={styles.filtros}>
           <TouchableOpacity
             style={styles.filtroConteudo}
@@ -50,14 +43,12 @@ const NoticesScreen = ({ navigation }) => {
             }}>
             <Text style={styles.filtroText}>Tipo</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.filtroConteudo}>
-            <Text
-              style={styles.filtroText}
-              onPress={() => {
-                setPressedDate(true);
-              }}>
-              Data
-            </Text>
+          <TouchableOpacity
+            style={styles.filtroConteudo}
+            onPress={() => {
+              setPressedDate(true);
+            }}>
+            <Text style={styles.filtroText}>Data</Text>
           </TouchableOpacity>
         </View>
         {pressedType ? (
@@ -74,15 +65,16 @@ const NoticesScreen = ({ navigation }) => {
               <NoticeCard
                 key={item.title}
                 cardProps={item}
-                navScreen={MessageScreen}
+                navScreen={"Message"}
               />
             ))}
           </ScrollView>
         </View>
-      </ImageBackground>
+      </View>
     </>
   );
 };
+
 NoticesScreen.navigationOptions = () => {
   return {
     headerShown: false,
@@ -90,27 +82,9 @@ NoticesScreen.navigationOptions = () => {
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 32,
-  },
   containerBackground: {
-    height: "100%",
-    width: "100%",
+    flex: 1,
     backgroundColor: "#3192b3",
-  },
-  scrollContainer: {},
-  imageBackground: {},
-  logo: {},
-  logoContainer: {
-    marginTop: 20,
-    alignItems: "center",
-  },
-  logoText: {
-    fontFamily: "MontserratBold",
-    fontWeight: "normal",
-    fontSize: 24,
-    color: "white",
-    marginTop: 30,
   },
   cardContainer: {
     marginTop: 15,
@@ -120,7 +94,7 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 30,
   },
   filtros: {
-    marginTop: 32,
+    // marginTop: 32,
     marginHorizontal: 30,
     flexDirection: "row",
     justifyContent: "space-between",
