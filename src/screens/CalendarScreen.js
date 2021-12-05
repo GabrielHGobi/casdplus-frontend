55146917bcb84228124759e6afaaea086efd6b3d
FernandoZanchitta <50930741+FernandoZanchitta@users.noreply.github.com>
2021-12-05 19:03:39 -0300

    Adding Packages for Calendar, and starting the calendar screen

--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -1,11 +1,25 @@
-import React from "react";
-import { RefreshControl, SafeAreaView, ScrollView, View, StyleSheet, Text } from "react-native";
+import React,{useState} from "react";
+import { RefreshControl,
+         SafeAreaView, 
+         ScrollView, 
+         View, 
+         StyleSheet, 
+         Text,
+         TouchableOpacity,
+         } from "react-native";
+
+import {Agenda} from 'react-native-calendars';
+import {Card,Avatar} from 'react-native-paper';
+
 import Header from "../components/Header";
 
 const wait = (timeout) => {
   return new Promise(resolve => setTimeout(resolve, timeout));
 }
-
+const timeToString = (time) => {
+  const date = new Date(time);
+  return date.toISOString().split('T')[0];
+};
 const CalendarScreen = () => {
   const [refreshing, setRefreshing] = React.useState(false);
 
@@ -14,16 +28,70 @@ const CalendarScreen = () => {
     wait(2000).then(() => setRefreshing(false));
   }, []);
 
+  const [items, setItems] = useState({});
+
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: 'Atividade de ' + strTime + ' #' + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };
+
+  const renderItem = (item) => {
+    return (
+      <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
+        <Card>
+          <Card.Content>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}>
+              <Text>{item.name}</Text>
+              <Avatar.Text label="Z" />
+            </View>
+          </Card.Content>
+        </Card>
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <SafeAreaView style = {styles.container}>
-      <ScrollView
-          contentContainerStyle={styles.scrollView}
-          refreshControl={ <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />  }
-        >
-          <Header title='Calendário do ano' />
-          <Text>Pull down to see RefreshControl indicator</Text>
-        </ScrollView>
-    </SafeAreaView>
+    // <SafeAreaView style = {styles.container}>
+    //   <ScrollView
+    //       contentContainerStyle={styles.scrollView}
+    //       refreshControl={ <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />  }
+    //     >
+    //       <Header title='Calendário do ano' />
+
+          <SafeAreaView style={{flex: 1}}>
+
+            <Agenda
+              items={items}
+              loadItemsForMonth={loadItems}
+              selected={'2017-05-16'}
+              renderItem={renderItem}
+            />
+          </SafeAreaView>
+    // </SafeAreaView>
   );
 };
 
